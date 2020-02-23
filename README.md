0723621646a088c69e1f417520bc2d1cec4bcf08
kilowat <kadantsev.sergey@gmail.com>
2020-02-23 20:29:21 +0300

    work with template

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-# Сборка webpack для простой верстки.
-## Команды:
-+ npm run dev - development
-+ npm run prod - production
-+ npm run server - start webserver and watch
-+ npm run watch - watch
+# Верстка макета для soc project
--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,20 @@
 import jQuery from "jquery";
+import Glide from '@glidejs/glide'
 import Vue from 'vue';
 import ExampleComponent from '../vue-components/ExampleComponent.vue';
 
+
+
+
 Vue.component('example-component', ExampleComponent );
+
 
 const vue_app = new Vue({
     el: '#app',
 });
 
 window.vue_app = vue_app;
+window.Glide = Glide;
 
 jQuery(function() {
   //jQuery("body").css("color", "blue");
