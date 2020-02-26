f422b7b13e9ddef70c57d4d746fde1321e4709d1
kilowat <kadantsev.sergey@gmail.com>
2020-02-26 15:30:19 +0300

    working

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,18 @@
 import $ from "jquery";
-import Glide from '@glidejs/glide'
+window.$ = $;
+window.jQuery = $;
 
+import Glide from '@glidejs/glide'
 import Vue from 'vue';
 import ExampleComponent from '../vue-components/ExampleComponent.vue';
+import { Slide } from 'vue-burger-menu';
 
 import '../lib/slick/slick.min.js';
 
-
+require('../lib/fancybox/jquery.fancybox.js');
 
 Vue.component('example-component', ExampleComponent );
-
+Vue.component('drawer-menu', Slide );
 
 const vue_app = new Vue({
     el: '#app',
@@ -17,7 +20,9 @@ const vue_app = new Vue({
 
 window.vue_app = vue_app;
 window.Glide = Glide;
-window.$ = $;
+
+
 $(function() {
+  $("#mobile-menu").show();
   //jQuery("body").css("color", "blue");
 });
